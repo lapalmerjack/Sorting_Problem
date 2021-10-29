dc2a24b3fa5054c5df6e71fc94a3389eb9b6c75f
BonaBeavis <gordian.dziwis@gmail.com>
2021-10-30 00:39:14 +0200

    I tend do be lazy

--- Main.java
+++ Main.java
@@ -1,100 +1,36 @@
 package com.company;
 
-import java.sql.Array;
 import java.util.Arrays;
-import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
-        // for testing
-        String[] array1 = new String[]{"Apple"};
-        String[] array2 = new String[]{"Apple", "happy"};
-        String[] array3 = new String[]{"Apple", "beer", "crow", "foot"};
-        String[] array4 = new String[]{"Apple", "shot"};
-        String[] array5 = new String[]{"Hungry", "happy"};
-        String[] array6 = new String[]{"Hungry", "pattern"};
-        String[] array7 = new String[]{"Apple", "shot"};
-        String[] array8 = new String[]{"Apple", "shot", "crow"};
-        String[] array9 = new String[]{"Apple", "beer", "crow", "foot"};
-        String[] array10 = new String[]{"Hungry", "pattern"};
-        String[] array11 = new String[]{"Hungry", "pattern"};
-        String[] array12 = new String[]{"Apple"};
-        String[] array13 = new String[]{"Hungry"};
-
         /*
-       [[Apple], [Apple], [Apple, beer, crow, foot], [Apple, beer, crow, foot], [Apple, happy], [Apple, shot],
-        [Apple, shot], [Apple, shot, crow], [Hungry], [Hungry, happy],
-       [Hungry, pattern], [Hungry, pattern], [Hungry, pattern]]
-
+         * [[Apple], [Apple], [Apple, beer, crow, foot], [Apple, beer, crow, foot], [Apple, happy],
+         * [Apple, shot], [Apple, shot], [Apple, shot, crow], [Hungry], [Hungry, happy], [Hungry,
+         * pattern], [Hungry, pattern], [Hungry, pattern]]
+         * 
          */
-
-
-
-        String[][] myArrays = {array2, array1, array4, array5, array6, array7,array8,array9, array10, array11, array12, array3, array13};
-
-
-        sortArrays(myArrays);
-        System.out.println(Arrays.deepToString(myArrays));
+        String[][] arrayOfArrays = {
+                {"Apple"},
+                {"Apple", "happy"},
+                {"Apple", "beer", "crow", "foot"},
+                {"Apple", "shot"},
+                {"Hungry", "happy"},
+                {"Hungry", "pattern"},
+                {"Apple", "shot"},
+                {"Apple", "shot", "crow"},
+                {"Apple", "beer", "crow", "foot"},
+                {"Hungry", "pattern"},
+                {"Hungry", "pattern"},
+                {"Apple"},
+                {"Hungry"}};
+        Arrays.sort(arrayOfArrays, (a, b) -> {
+            String joinedA = String.join("", a);
+            String joinedB = String.join("", b);
+            return joinedA.compareTo(joinedB);
+        });
+        System.out.println(Arrays.deepToString(arrayOfArrays));
     }
-
-    public static String [] returnMinLengthArray(String [] firstArray, String [] secondArray) {
-        if (firstArray.length > secondArray.length) {
-            return secondArray;
-        } else {
-            return firstArray;
-        }
-    }
-
-    public static String [] compareTwoArrays(String [] firstArray, String [] secondArray) {
-
-
-        String[] arrayReturn = firstArray;
-        if(Arrays.equals(firstArray, secondArray)) {
-            return arrayReturn;
-        } else {
-            int minLength = findMinLength(firstArray, secondArray);
-            for(int i = 0; i < minLength; i++) {
-
-               if(secondArray[i].compareToIgnoreCase(firstArray[i] ) > 0) {
-                    arrayReturn = firstArray;
-
-                    break;
-                } else if (secondArray[i].compareToIgnoreCase(firstArray[i] ) < 0) {
-                   arrayReturn = secondArray;
-
-                   break;
-                } else if(i+ 1 == minLength && (firstArray.length > minLength || secondArray.length > minLength)) {
-                   arrayReturn = returnMinLengthArray(firstArray, secondArray);
-
-               }
-
-            }
-        }
-        return arrayReturn;
-    }
-
-    public static void sortArrays(String [][] myArrays) {
-        String[] temp;
-        for (int i = 0; i < myArrays.length; i++) {
-            for (int j = i + 1; j < myArrays.length; j++) {
-                temp = compareTwoArrays(myArrays[j], myArrays[i]);
-                if(findArrayMatch(temp, myArrays[j])) {
-                    myArrays[j] = myArrays[i];
-                    myArrays[i] = temp;
-                }
-            }
-        }
-    }
-
-    public static boolean findArrayMatch(String[] temp, String[] firstArray) {
-        return Arrays.equals(temp, firstArray);
-
-    }
-
-    public static int findMinLength(String [] firstArray, String [] secondArray) {
-        return Math.min(firstArray.length, secondArray.length);
-    }
-
 
 }
